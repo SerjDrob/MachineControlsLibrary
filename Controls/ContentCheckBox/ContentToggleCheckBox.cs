7cba8ec0707cc37f08163706c89aa71397d030f0
Сергей Дробышев <serjdrob@gmail.com>
2026-05-22 09:54:09 +0700

    feat: added ContentToggleCheckBox

---
+++ Controls/ContentCheckBox/ContentToggleCheckBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MachineControlsLibrary.Controls.ContentCheckBox;
+
+public class ContentToggleCheckBox : CheckBox
+{
+    static ContentToggleCheckBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(ContentToggleCheckBox),
+            new FrameworkPropertyMetadata(typeof(ContentToggleCheckBox)));
+    }
+
+    public object CheckedContent
+    {
+        get => GetValue(CheckedContentProperty);
+        set => SetValue(CheckedContentProperty, value);
+    }
+    public static readonly DependencyProperty CheckedContentProperty =
+        DependencyProperty.Register(nameof(CheckedContent), typeof(object), typeof(ContentToggleCheckBox), new PropertyMetadata(null));
+
+    public object UncheckedContent
+    {
+        get => GetValue(UncheckedContentProperty);
+        set => SetValue(UncheckedContentProperty, value);
+    }
+    public static readonly DependencyProperty UncheckedContentProperty =
+        DependencyProperty.Register(nameof(UncheckedContent), typeof(object), typeof(ContentToggleCheckBox), new PropertyMetadata(null));
+}
